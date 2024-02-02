188aa0abc512b61b83768d1f9113f21bd12540e0
Adam Walton <adamrwalton@gmail.com>
2024-02-02 21:47:54 +0200

    Add basic serializers to Django

--- foodBankBackend/foodbankapi/serializers.py
+++ foodBankBackend/foodbankapi/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from .models import *
 
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Address
+        fields=["street", "city", "state", "country", "postalCode"]
+
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model=Item
@@ -21,3 +27,21 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model=Order
         fields=["creationDate", "expirationDate", "status", "orderItems"]
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
+    class Meta:
+        model=Organization
+        fields=["name", "address", "phoneNumber", "type"]
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=["username", "first_name", "last_name", "email", ]
+
+class ConnectionRequestSerializer(serializers.ModelSerializer):
+    fromOrganization = OrganizationSerializer()
+    toOrganization = OrganizationSerializer()
+    class Meta:
+        model=ConnectionRequest
+        fields=["fromOrganization", "toOrganization"]
