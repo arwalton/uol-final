21bb9800f860a5a9ee22b4170b2d20e1117acfed
Adam Walton <adamrwalton@gmail.com>
2024-02-06 17:40:36 +0200

    Register models on Admin site

--- foodBankBackend/foodbankapi/admin.py
+++ foodBankBackend/foodbankapi/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-
+admin.site.register(Address)
 admin.site.register(Item)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Organization)
+admin.site.register(User)
+admin.site.register(ConnectionRequest)
